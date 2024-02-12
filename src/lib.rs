2345dd4d0b2aaa30319095ad4eae28ebb89770ac
Ping <ping47@gmail.com>
2024-02-12 22:20:39 +0800

    minigrap

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,44 @@
-use std::{error::Error, fs};
+use std::{env, error::Error, fs};
 
 pub struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("No query string"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("No file path"),
+        };
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &content) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &content)
+    } else {
+        search(&config.query, &content)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -29,14 +46,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &'a str, content: &'a str) -> Vec<&'a str> {
-    let mut stack = Vec::new();
+    content
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
 
-    for line in content.lines() {
-        if line.contains(query) {
-            stack.push(line);
-        }
-    }
-    stack
+pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    content
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
 }
 
 #[cfg(test)]
@@ -51,5 +71,20 @@ Rust:
 safe, fast, productive.
 Pick three.";
         assert_eq!(vec!["safe, fast, productive."], search(query, content));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ use std::{env, process};
 use ripripgrep::{run, Config};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = env::args();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = Config::build(args).unwrap_or_else(|err| {
+        eprintln!("[ERROR] Problem parsing arguments: {err}");
         process::exit(1)
     });
 
     if let Err(e) = run(config) {
-        println!("[ERROR] {e}");
+        eprintln!("[ERROR] {e}");
         process::exit(1)
     }
 }
